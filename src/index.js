bc00eaf3eaf0b787a942f36a902a34a399e83c1c
Eduardo Lopes <eduardotorreslopes@gmail.com>
2020-01-28 19:12:19 -0300

    add link to schema and resolvers

--- src/index.js
+++ src/index.js
@@ -1,8 +1,20 @@
 const { GraphQLServer } = require('graphql-yoga')
 
+let links = [{
+  id: 'link-0',
+  url: 'www.howtographql.com',
+  description: 'Fullstack tutorial for GraphQL'
+}]
+
 const resolvers = {
   Query: {
-    info: () => `This is the API of a Hackernews Clone`
+    info: () => `This is the API of a Hackernews Clone`,
+    feed: () => links,
+  },
+  Link: {
+    id: parent => parent.id,
+    description: (parent) => parent.description,
+    url: (parent) => parent.url,
   }
 }
 
